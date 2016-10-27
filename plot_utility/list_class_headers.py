42d8cd69bd30c914130e8baa45cb6a137fedce6c
ardok-m <ardok@openmailbox.org>
2016-10-27 15:04:17 +0200

    list_variables strip white space before returning h_list

--- plot_utility/list_class_headers.py
+++ plot_utility/list_class_headers.py
@@ -13,17 +13,19 @@ def list_variables(filename):
                 break
             prev_line = line
 
-    list = re.split('\d+:', prev_line)[1:]  # Remove '#'
-    list = map(str.strip, list)  # Remove extra spaces
-    list = zip(range(len(list)), list)
+    h_list = re.split('\d+:', prev_line)[1:]  # Remove '#'
+    h_list = map(str.strip, h_list)
 
-    return list
+    return h_list
 
 
-def print_beautifully(list):
+def print_beautifully(h_list):
+
+    h_list = zip(range(len(h_list)), h_list)
+
     print "('Column', 'Variable')\n"
 
-    for entry in list:
+    for entry in h_list:
         print entry
 
     print "\nRecall that (.) = 8piG/3"
@@ -37,11 +39,11 @@ def main():
         description="Read the output of CLASS and return a list of column\
         numbers and their associated variables.")
 
-    parser.add_argument("file", help="Path to file.")
+    parser.add_argument("filepath", help="Path to file.")
 
     args = parser.parse_args()
 
-    print_beautifully(list_variables(args.file))
+    print_beautifully(list_variables(args.filepath))
 
     return 1
 
