012395e1f76e752343af8ea114bb9b41d45c0a1d
ardok-m <ardok@openmailbox.org>
2016-11-18 17:46:09 +0100

    fixed label and scale option. added title option

--- quick_columns_plot.py
+++ quick_columns_plot.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def plot(filename, x=0, y=1, x_abs=False, y_abs=False, x_scale='linear',
-         y_scale='linear', x_label='x', y_label='y', scatter=False, size=10):
+         y_scale='linear', x_label='x', y_label='y', scatter=False, size=10,
+         title=''):
 
     x, y = np.loadtxt(filename, usecols=(x, y), unpack=True)
 
@@ -15,10 +16,13 @@ def plot(filename, x=0, y=1, x_abs=False, y_abs=False, x_scale='linear',
         y = np.fabs(y)
 
     f, ax = plt.subplots(1, 1)
-    ax.x_scale(x_scale)
-    ax.y_scale(y_scale)
-    ax.x_label(x_label)
-    ax.y_label(y_label)
+    ax.set_xscale(x_scale)
+    ax.set_yscale(y_scale)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    if title:
+        plt.title(title)
 
     if scatter:
         ax.scatter(x, y, s=size)
@@ -26,4 +30,3 @@ def plot(filename, x=0, y=1, x_abs=False, y_abs=False, x_scale='linear',
         ax.plot(x, y, linewidth=size)
 
     plt.show()
-
