59fb9373a24578546b247d74d105df61b1a4086a
ardok-m <ardok@openmailbox.org>
2016-11-23 12:33:28 +0100

    added support for lines and xy projection in 3d

--- quick_columns_plot.py
+++ quick_columns_plot.py
@@ -3,7 +3,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 
 
 def plot(filename, x=0, y=1, x_abs=False, y_abs=False,
@@ -32,6 +31,7 @@ def plot(filename, x=0, y=1, x_abs=False, y_abs=False,
         ax.plot(x, y, linewidth=size)
 
     plt.show()
+    plt.close()
 
 
 def plot_color(filename, x=0, y=1, c=2, x_abs=False, y_abs=False,
@@ -70,16 +70,19 @@ def plot_color(filename, x=0, y=1, c=2, x_abs=False, y_abs=False,
     plt.axis([xmin, xmax, ymin, ymax])
 
     plt.show()
+    plt.close()
 
 
 def plot_3d(filename, x=0, y=1, z=2, c=None, x_abs=False, y_abs=False,
             z_abs=False, x_scale='linear', y_scale='linear', z_scale='linear',
             x_label='col_0', y_label='col_1', z_label='None', c_label='',
             size=30, title='', vmax=None, vmin=None, cmap=1, xmin=None,
-            xmax=None, ymin=None, ymax=None):
+            xmax=None, ymin=None, ymax=None, zmin=None, zmax=None,
+            depthshade=0, xy_plot=False, line_ini=None, line_end=None,
+            line_label=None):
 
     fig = plt.figure()
-    ax = Axes3D(fig)
+    ax = fig.add_subplot(111, projection='3d')
 
     colormap = {
         1: cm.coolwarm,
@@ -87,7 +90,6 @@ def plot_3d(filename, x=0, y=1, z=2, c=None, x_abs=False, y_abs=False,
         3: cm.Blues,
         4: cm.afmhot,
     }
-
     if c is not None:
         cmap = colormap[cmap]
         x, y, z, c = np.loadtxt(filename, usecols=(x, y, z, c), unpack=True)
@@ -102,7 +104,22 @@ def plot_3d(filename, x=0, y=1, z=2, c=None, x_abs=False, y_abs=False,
     if z_abs:
         z = np.fabs(z)
 
-    cax = ax.scatter(x, y, z, s=size, c=c, cmap=cmap, vmax=vmax, vmin=vmin)
+    cax = ax.scatter(x, y, z, s=size, c=c, cmap=cmap, vmax=vmax, vmin=vmin,
+                     depthshade=depthshade)
+
+    if xy_plot:
+        zmin_aux = zmin or ax.set_zlim3d()[0]
+        ax.plot(x, y, zmin_aux)
+
+    # Consider allowing any kind of line. ATM just straight lines.
+    if (line_ini is not None) and (line_end is not None):
+        line = zip(line_ini, line_end)
+        ax.plot(line[0], line[1], line[2], linewidth=0.05 * size, c='black',
+                label=line_label)
+    elif ((line_ini is not None) or
+          (line_end is not None)) and ((line_ini is None) or (line_end is
+                                                              None)):
+        print "Line not drawn. You must enter the initial and end points"
 
     ax.set_title(title)
     ax.set_xscale(x_scale)
@@ -115,6 +132,11 @@ def plot_3d(filename, x=0, y=1, z=2, c=None, x_abs=False, y_abs=False,
     cbar = fig.colorbar(cax)
     cbar.set_label(c_label)
 
-    plt.axis([xmin, xmax, ymin, ymax])
+    ax.set_xlim3d([xmin, xmax])
+    ax.set_ylim3d([ymin, ymax])
+    ax.set_zlim3d([zmin, zmax])
+
+    ax.legend()
 
     plt.show()
+    plt.close()
