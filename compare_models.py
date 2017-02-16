212ea1bb4f6651f0323fadefb6b4497afdd23c4e
ardok-m <ardok@openmailbox.org>
2017-02-16 13:21:16 +0100

    added relative and absolute deviations

--- compare_models.py
+++ compare_models.py
@@ -3,10 +3,11 @@
 #from classy import Class
 from matplotlib import pyplot as plt
 import numpy as np
+import wicmath
 
 ######
 # This program must be versatile. It will accept classy dictionaries to
-# compare. It should be use together with classy.
+# compare. It should be used together with classy.
 ######
 
 
@@ -14,6 +15,9 @@ class Compare():
     def __init__(self, x, *args):
         if self.__check_dicts(*args): self._dicts = args
         if self.__check_x(x): self._x= x
+        self._adev = ()
+        self._rdev = ()
+        self.__refdict = self._dicts[0]
 
     def __check_x(self, *args):
         if args:
@@ -37,6 +41,49 @@ class Compare():
                 # return False
             return True
 
+    def __select_key(self, y):
+        if y == 'all':
+            keys = self.__refdict.iterkeys()
+        else:
+            keys = [y]
+
+        return keys
+
+    def __deviation(self, y, kind):
+        ref = self.__refdict
+        x = self._x
+        keys = self.__select_key(y)
+
+        if kind == 'rel':
+            deviation = wicmath.relative_deviation
+        elif kind == 'abs':
+            deviation = wicmath.absolute_deviation
+        else:
+            raise ValueError("kind must be one of 'rel or 'abs'")
+
+        output_dicts = []
+
+        for adict in self._dicts[1:]:
+            d = {}
+            for akey in keys:
+                d[akey] =  deviation(adict[x], adict[akey], ref[x], ref[akey])
+            output_dicts.append(d)
+
+        return tuple(output_dicts)
+
+    def relative_deviation(self, y='all'):
+        """Returns the percentual relative deviation between the common
+        dictionaries variables or the specified variable if y is given. The
+        fist dictionary is taken as reference"""
+        self._rdev = self.__deviation(y, 'rel')
+        return self._rdev
+
+    def absolute_deviation(self, y='all'):
+        """Returns the absolute deviation between the common dictionaries
+        variables or the specified variable if y is given"""
+        self._adev = self.__deviation(y, 'abs')
+        return self._adev
+
     def update_x(self, x):
         self.__check_x(x)
         self._x = x
@@ -46,14 +93,6 @@ class Compare():
         self._dicts = args
         self.__check_x()
 
-    def relative_deviation(self):
+    def plot(self, y="all"):
         pass
-
-    def absolute_deviation(self):
-        pass
-
-    def plot(self, which="all"):
-        pass
-
-
 
