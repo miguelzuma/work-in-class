39b5841b46ca01f9f168974467cf83c111841c16
ardok-m <carlosgarcia@iff.csic.es>
2018-02-07 14:17:35 +0100

    removed means,sigma computation, added method for correlation

--- histograms.py
+++ histograms.py
@@ -14,6 +14,7 @@ class Histograms():
         self.bins = []
         self.means = []
         self.sigmas = []
+        self.correlations = []
 
     def read_file(self, fname, header=True):
         """
@@ -45,13 +46,13 @@ class Histograms():
 
         self.histograms = histograms
 
-    def _compute_means(self):
+    def compute_means(self):
         """
         Compute the mean for each bin.
         """
         self.means = np.mean(self.data, axis=1)
 
-    def _compute_sigmas(self):
+    def compute_sigmas(self):
         """
         Compute the intervals that contains the 68% of the data around the means.
         """
@@ -86,13 +87,35 @@ class Histograms():
 
         self.sigmas = sigmas
 
+    def compute_correlations(self):
+        """
+        Compute all correlations between variables.
+        """
+        # TODO: Use symmetry to speed up the calc.
+        for i in self.bins:
+            self.compute_correlation_bin(i)
+
+    def compute_correlation_bin(self, zbin):
+        """
+        Compute <w(zbin)w_i> correlation values.
+        """
+        if self.means == []:
+            self.compute_means()
+
+        if self.correlations == []:
+            self.correlations = [np.nan] * len(self.bins)
+
+        wzbin_half = self.data[zbin] - self.means[zbin]
+
+        self.correlations[zbin] = np.mean([wzbin_half*(w - w_mean) for w, w_mean
+                                           in zip(self.data, self.means)],
+                                          axis=1)
+
     def compute(self, bins='auto'):
         """
-        Compute histograms, means and sigmas
+        Compute histograms
         """
         self._compute_histograms(bins)
-        self._compute_means()
-        self._compute_sigmas()
 
     def plot_histogram(self, index, variable_binned='bin_i', xlabel='x', xlim=[None, None]):
         """
@@ -101,16 +124,19 @@ class Histograms():
         data = self.data[index].copy()
         data.sort()
         histogram = self.histograms[index]
-        mean = self.means[index]
-        sigma = self.sigmas[index]
 
         bins = histogram[1][:-1]
         density = histogram[0]/histogram[0].sum(dtype=float)
 
         plt.bar(bins, density, width=np.diff(histogram[1]), color='b')
         plt.plot(bins, density, c='r')
-        plt.axvline(mean, c='black', label='mean')
-        plt.axvspan(mean - sigma[0], mean + sigma[1], alpha=0.5, color='grey', label='68% data')
+
+        if self.means:
+            mean = self.means[index]
+            plt.axvline(mean, c='black', label='mean')
+            if self.sigmas:
+                sigma = self.sigmas[index]
+                plt.axvspan(mean - sigma[0], mean + sigma[1], alpha=0.5, color='grey', label='68% data')
 
         plt.xlim(xlim)
         plt.xlabel(xlabel)
